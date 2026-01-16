2a2c7113c5a9393687796fed1268db14ec190f56
Harry Shaper <harryshaper@gmail.com>
2026-01-16 12:19:57 +0000

    App details added

--- 5_app/data_sort_v01.py
+++ 5_app/data_sort_v01.py
@@ -8,9 +8,21 @@ author      Harry Shaper <harryshaper@gmail.com>
 
 *************************************************'''
 
-'''Types of data to sort
+'''
+
+Types of data to sort
 -HDRI
 -PANO
 -Texture Ref
 -Set Ref 
--Photogrammetry'''
+-Photogrammetry
+
+App layout:
+1. User inputs details (Project, name, current date)
+2. User selects the folder they want to sort
+3. User inputs more details:
+   Slate
+   Location
+   Time - Pulled from MetaData
+   Focal Length - Pulled from MetaData
+
